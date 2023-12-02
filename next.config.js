8330c5da95ce3201aeab399ffb6dacbacc55267b
yoshimo <109893020+yoshi-mo@users.noreply.github.com>
2023-12-02 17:23:49 +0900

    final

--- next.config.js
+++ next.config.js
@@ -1,8 +1,24 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    appDir: true,
+  webpack: (config, { buildId, dev, isServer, defaultLoaders, nextRuntime, webpack }) => {
+    config.externals.push({
+      "utf-8-validate": "commonjs utf-8-validate",
+      bufferutil: "commonjs bufferutil"
+    })
+
+    config.module.rules.push({
+      test: /\.mjs$/,
+      include: /node_modules/,
+      type: "javascript/auto",
+    });
+
+    return config
   },
+  images: {
+    domains: [
+      "utfs.io"
+    ]
+  }
 }
 
 module.exports = nextConfig
